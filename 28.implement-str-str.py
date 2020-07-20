6d5120b18bec2a15ae2786d9f83634a8c54e7f45
dely2p <lhyal2p@gmail.com>
2020-07-20 22:24:05 +0900

    add 28.implement-str-str.py

---
+++ 28.implement-str-str.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=28 lang=python3
+#
+# [28] Implement strStr()
+#
+
+# @lc code=start
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            return haystack.index(needle)
+        else:
+            return -1
+# @lc code=end
+
