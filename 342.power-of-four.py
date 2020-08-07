b769694fa48f5a87732ee53d63e62954452384f9
dely2p <lhyal2p@gmail.com>
2020-08-07 22:38:32 +0900

    add 342.power-of-four.py

---
+++ 342.power-of-four.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=342 lang=python3
+#
+# [342] Power of Four
+#
+
+# @lc code=start
+class Solution:
+    def isPowerOfFour(self, num: int) -> bool:
+        list = [4, 16, 64, 256, 1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216, 67108864, 268435456, 1073741824]
+        if num in list or num == 1:
+            return True
+        else:
+            return False
+
+# @lc code=end
+
